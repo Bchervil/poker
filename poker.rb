c5be2b1ac6075d213687ad783481b68aeae2fb60
Beerscheba chervil <designergo@aol.com>
2019-03-19 17:53:14 -0400

    first commit

---
+++ poker.rb
@@ -0,0 +1,6 @@
+class Card
+	FACES = ['2', '3', '4', '5', '6', '7', '8', '9', '10',
+			 'jack', 'queen', 'king', 'ace']
+	SUITS = ['hearts', 'clubs', 'diamonds', 'spades']
+
+	attr_reader :rank, :suit, :face
